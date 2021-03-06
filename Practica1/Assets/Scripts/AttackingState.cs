525fc11c7b4e78233c9c85913e834ad0472c30af
iSeyren <79010952+iSeyren@users.noreply.github.com>
2021-03-06 07:30:49 -0600

    Agente

---
+++ Practica1/Assets/Scripts/AttackingState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingState : FunctionsFSM
+{
+    public override void EnterState(AgenteEstatico agent)
+    {
+        //Código para cuando entra al estado de atacar
+    }
+
+    public override void UpdateState(AgenteEstatico agent)
+    {
+        //Código para cuando no se detecta al jugador
+    }
+}
